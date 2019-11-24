f3b2ca1ca85f3cba46da859e4e40b5c720170aca
Łukasz Drabek <lpdrabek@gmail.com>
2019-11-24 13:31:16 +0100

    test_company_name

--- test_mac_provider.py
+++ test_mac_provider.py
@@ -6,6 +6,7 @@ import responses
 import mac_provider
 
 mac = "443839ffef57"
+company_name = "Cumulus Networks, Inc"
 
 API_MOCK='https://api.macaddress.io/v1?apiKey={}&output=json&search={}'.format(os.environ['API_KEY'], mac)
 API_RESPONSE='{"vendorDetails":{"oui":"443839","isPrivate":false,"companyName":"Cumulus Networks, Inc","companyAddress":"650 Castro Street, suite 120-245 Mountain View CA 94041 US","countryCode":"US"},"blockDetails":{"blockFound":true,"borderLeft":"443839000000","borderRight":"443839FFFFFF","blockSize":16777216,"assignmentBlockSize":"MA-L","dateCreated":"2012-04-08","dateUpdated":"2015-09-27"},"macAddressDetails":{"searchTerm":"443839ffef57","isValid":true,"virtualMachine":"Not detected","applications":["Multi-Chassis Link Aggregation (Cumulus Linux)"],"transmissionType":"unicast","administrationType":"UAA","wiresharkNotes":"No details","comment":""}}'
@@ -25,3 +26,6 @@ def test_make_request_bad():
     resp = mac_provider.make_request(mac)
 
     assert resp == False
+
+def test_get_company_name():
+    assert company_name == mac_provider.get_company_name(json.loads(API_RESPONSE))
