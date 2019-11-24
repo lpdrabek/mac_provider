043ceb0429743e03a8c19060ef949d2c7a8d6ec3
Łukasz Drabek <lpdrabek@gmail.com>
2019-11-24 11:57:20 +0100

    prepare_mac - remove common separators

--- mac_provider.py
+++ mac_provider.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
-print("hello_world!")
+mac = "44:38:39:ff:ef:57"
+
+def prepare_mac(mac_address):
+    parsed_mac = mac_address.replace(":", "")
+    parsed_mac = parsed_mac.replace("-", "")
+    parsed_mac = parsed_mac.replace(".", "")
+
+    return parsed_mac
+
+
+print(prepare_mac(mac))
